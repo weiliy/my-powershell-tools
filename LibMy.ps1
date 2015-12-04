e9021a141fe4ed3d94e2a03bd81772c35ecfe3fe
Weili <yi@weiliy.net>
2015-12-04 14:04:15 +0800

    feat: Out-MyLog

---
+++ LibMy.ps1
@@ -0,0 +1,42 @@
+﻿# Write Log to file and screen
+Function Out-MyLog {
+    <#
+    .SYNOPSIS
+           Write Log to file and screen
+    #>
+    Param([string]$LogFileName = '')
+    Begin{
+        If ( $LogFileName -ne '' ) {
+            $UtcTime = (Get-Date).ToUniversalTime() | Get-Date -UFormat '%Y-%m-%d %H:%M (UTC)'
+            If ( Test-Path $LogFileName ) {
+                Write-Output "$LogFileName already exists, new log will append the end of it"  
+            } Else {
+                Write-Output "Creating logfile $LogFileName"  
+                New-Item -Path $LogFileName -ItemType file | Out-Null
+            }
+            $Script:MyLogFileName = $LogFileName
+            "Logging start at $UtcTime `n" | Add-Content $Script:MyLogFileName
+        }
+        If ( $Script:MyLogBuffer -eq $null ) {
+            $Script:MyLogBuffer = @()
+        }
+    }
+    Process {
+        $UtcTime = (Get-Date).ToUniversalTime() | Get-Date -UFormat '%Y-%m-%d %H:%M:%S'
+        $LogMsg =  $UtcTime + ': ' + $_ 
+        $Script:MyLogBuffer += $LogMsg
+
+        Write-Output $LogMsg
+    }
+    End {
+        
+        try {
+            $Script:MyLogBuffer | Add-Content $Script:MyLogFileName
+        } catch {
+            "Cannot write log into $MyLogFileName"
+            Read-Host "Press Enter to exit"
+            Exit
+        } 
+        $Script:MyLogBuffer = $null
+    }
+}
