ce13b2c72494e6ba3964cb1d3bf300f5f11d61c5
Weili <yi@weiliy.net>
2015-12-04 17:27:52 +0800

    feat: setup script to load libs

---
+++ Setup.ps1
@@ -0,0 +1,38 @@
+﻿# Initialize the Env
+Function Initialize-MyEnv {
+    <#
+    .SYNOPSIS
+	    Initialize the Env
+    .VERSION
+	    0.1
+    .EXAMPLE
+        1. Load the libery
+	    Initialize-MyEnv -MyScript $MyInvocation.MyCommand.Definition
+
+        2. Load with specific lib folder name
+        Initialize-MyEnv -Lib 'AnotherLib' -MyScript $MyInvocation.MyCommand.Definition
+    #>
+    Param (
+        [string]$MyScript = $(throw "Parameter missing: -MyScript The Script Full Name"),
+        [String]$Lib = 'lib'
+    )
+    $Script:MyScriptFullName = $MyScript
+    $Script:MyScriptName = Split-Path $MyScriptFullName -Leaf 
+    $Script:MyScriptRoot = Split-Path $MyScriptFullName -Parent
+ 
+    # Read Libs
+    If ( -not $NotLoadLib.IsPresent ) {
+        $libPath =  Join-Path -ChildPath $Lib -Path $MyScriptRoot
+        $Script:Libs = Get-ChildItem $libPath | Where-Object { 
+            $_.Name -match '.ps1$' 
+        } | foreach {
+            Join-Path $libPath $_
+        }
+    }
+}
+
+Initialize-MyEnv -MyScript $MyInvocation.MyCommand.Definition
+# Load Libs
+foreach ( $Lib in $Libs ) {
+    . $Lib
+}
